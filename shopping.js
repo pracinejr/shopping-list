797ce2d5de4cc9a799f682ca598fa9ae4edc5341
Peter Racine Jr <peterracinejr@gmail.com>
2021-06-01 18:44:17 -0500

    initial grocery list

---
+++ shopping.js
@@ -0,0 +1,54 @@
+const groceryList = [
+  {
+    id: 1,
+    name: "milk",
+    price: 3.5,
+  },
+  {
+    id: 2,
+    name: "eggs",
+    price: 6,
+  },
+  {
+    id: 3,
+    name: "pizza rolls",
+    price: 12.25,
+  },
+  {
+    id: 4,
+    name: "ground beef",
+    price: 10.99,
+  },
+  {
+    id: 5,
+    name: "bananas",
+    price: 4.5,
+  },
+  {
+    id: 6,
+    name: "everything bagels",
+    price: 8.75,
+  },
+  {
+    id: 7,
+    name: "mayo",
+    price: 5.95,
+  },
+  {
+    id: 8,
+    name: "chicken",
+    price: 9,
+  },
+  {
+    id: 9,
+    name: "jalapenos",
+    price: 25.99,
+  },
+  {
+    id: 10,
+    name: "avocados",
+    price: 5.95,
+  },
+];
+
+console.log(groceryList);
